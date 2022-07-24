382ec3be379d71adf7ac8603361041e927423fed
lgd <1017276522@qq.com>
2022-07-24 15:46:36 +0800

    LeeCode +1;

---
+++ src/easy_compute/easyHuiWen.java
@@ -0,0 +1,26 @@
+package easy_compute;
+
+import java.util.Locale;
+
+/**
+ * @Description:    有效回文    使用正则表达去除字符中除去字母与数字
+ * 执行用时：13 ms, 在所有 Java 提交中击败了7.73%的用户
+ * 内存消耗：42 MB, 在所有 Java 提交中击败了8.22%的用户
+ * @author: LGD
+ * @date:2022/7/24 15:32
+ */
+public class easyHuiWen {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        String replace = s.toLowerCase().replaceAll("[^a-z0-9]+", "");
+        for (int i = 0; i < replace.length()/2; i++) {
+            if (replace.charAt(i) != replace.charAt(replace.length()-i-1))
+                return false;
+        }
+        return true;
+    }
+}
