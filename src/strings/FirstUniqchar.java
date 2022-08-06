6bc58fd725e6e574e6c8db9f0a9355da9b810907
lgd <1017276522@qq.com>
2022-08-06 20:16:39 +0800

    LeeCode+1

--- src/strings/FirstUniqchar.java
+++ src/strings/FirstUniqchar.java
@@ -1,6 +1,9 @@
 package strings;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Description:    第一个只出现一次的 , 直接字典
@@ -11,8 +14,10 @@ import java.util.HashMap;
  */
 public class FirstUniqchar {
     public static void main(String[] args) {
-        System.out.println(firstUniqChar("lgggddd"));
+//        System.out.println(firstUniqChar("lgggddd"));
+        System.out.println(reverseWords("a good   example"));
     }
+
     public static char firstUniqChar(String s) {
         if(s == null || s.length() == 0)
             return ' ';
@@ -26,6 +31,22 @@ public class FirstUniqchar {
             }
         }
         return ' ';
+    }
+
+    /**
+     * 反转字符
+     * 执行用时：5 ms, 在所有 Java 提交中击败了33.88%的用户
+     * 内存消耗：41.8 MB, 在所有 Java 提交中击败了11.11%的用户
+     */
+    public static String reverseWords(String s) {
+        String[] s1 = s.trim().split("\\s+");
+        List<String> list = Arrays.stream(s1).collect(Collectors.toList());
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = list.size()-1;i>0;i--){
+            stringBuilder.append(list.get(i) + " ");
+        }
+        stringBuilder.append(list.get(0));
+        return stringBuilder.toString();
     }
 
 }
