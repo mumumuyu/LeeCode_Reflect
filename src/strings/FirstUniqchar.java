62c73e554ebf1eaea68b1d4d4af9be0d53a0c147
lgd <1017276522@qq.com>
2022-08-07 21:14:03 +0800

    LeeCode+1

--- src/strings/FirstUniqchar.java
+++ src/strings/FirstUniqchar.java
@@ -1,8 +1,6 @@
 package strings;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -15,7 +13,8 @@ import java.util.stream.Collectors;
 public class FirstUniqchar {
     public static void main(String[] args) {
 //        System.out.println(firstUniqChar("lgggddd"));
-        System.out.println(reverseWords("a good   example"));
+//        System.out.println(reverseWords("a good   example"));
+        System.out.println(reverseLeftWords(new String("lgd123456"), 2));
     }
 
     public static char firstUniqChar(String s) {
@@ -40,13 +39,22 @@ public class FirstUniqchar {
      */
     public static String reverseWords(String s) {
         String[] s1 = s.trim().split("\\s+");
-        List<String> list = Arrays.stream(s1).collect(Collectors.toList());
-        StringBuilder stringBuilder = new StringBuilder();
-        for(int i = list.size()-1;i>0;i--){
+//        List<String> list = Arrays.stream(s1).collect(Collectors.toList());
+        Collections.reverse(Arrays.asList(s1));
+//        StringBuilder stringBuilder = new StringBuilder();
+        /*for(int i = list.size()-1;i>0;i--){
             stringBuilder.append(list.get(i) + " ");
         }
-        stringBuilder.append(list.get(0));
-        return stringBuilder.toString();
+        stringBuilder.append(list.get(0));*/
+//        return stringBuilder.toString();
+        return String.join(" ",s1);
     }
 
+    /** 左旋字符串
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：41.8 MB, 在所有 Java 提交中击败了7.19%的用户
+     */
+    public static String reverseLeftWords(String s, int n) {
+        return String.join("",s.substring(n),s.substring(0,n));
+    }
 }
