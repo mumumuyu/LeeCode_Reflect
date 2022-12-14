23ebd533cff45ee26258aca3d5cfa1391c4ad0e1
lgd <2709296991@qq.com>
2022-12-14 10:57:11 +0800

    +1

---
+++ src/array/FeiboNaqi.java
@@ -0,0 +1,37 @@
+package array;
+
+//编程尽头是数学= =，之后看看咋写的，基本矩阵操作
+public class FeiboNaqi {
+    static final int MOD = 1000000007;
+
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int[][] q = {{1, 1}, {1, 0}};
+        int[][] res = pow(q, n - 1);
+        return res[0][0];
+    }
+
+    public int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0}, {0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+
+    public int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                c[i][j] = (int) (((long) a[i][0] * b[0][j] + (long) a[i][1] * b[1][j]) % MOD);
+            }
+        }
+        return c;
+    }
+}
--- src/treeNodeTest/BuildTree.java
+++ src/treeNodeTest/BuildTree.java
@@ -1,20 +1,38 @@
 package treeNodeTest;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description:    前中序构建树
  * 执行用时：8 ms, 在所有 Java 提交中击败了13.59%的用户
  * 内存消耗：86.3 MB, 在所有 Java 提交中击败了5.00%的用户
+ *
+ * 利用Map哈希扫描，存储中序结点位置 buildTree3
+ * 执行耗时:1 ms,击败了99.92% 的Java用户
+ * 内存消耗:40.9 MB,击败了88.22% 的Java用户
  * @author: LGD
  * @date:2022/8/1 10:02
  */
 public class BuildTree {
+
+    private static Map<Integer, Integer> indexMap;
+
     public static void main(String[] args) {
         int[] pre = {3,9,20,15,7};
         int[] in = {9,3,15,20,7};
         TreeNode treeNode = buildTree(pre, in);
+//        TreeNode treeNode = buildTreeTest(pre, in);
         TreeNode.show(treeNode);
+    }
+
+    public static TreeNode buildTreeTest(int[] preorder, int[] inorder) {
+        indexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            indexMap.put(inorder[i],i);
+        }
+        return buildTree(preorder,inorder);
     }
 
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
@@ -22,12 +40,14 @@ public class BuildTree {
         if (n == 0)
             return null;
         int rootVal = preorder[0], rootIndex = 0;
+        //找出根节点所在中序位置
         for (int i = 0; i < n; i++) {
             if (inorder[i] == rootVal) {
                 rootIndex = i;
                 break;
             }
         }
+//        rootIndex = indexMap.get(rootVal);
         TreeNode root = new TreeNode(rootVal);
         root.left = buildTree(Arrays.copyOfRange(preorder, 1, 1 + rootIndex), Arrays.copyOfRange(inorder, 0, rootIndex));
         root.right = buildTree(Arrays.copyOfRange(preorder, 1 + rootIndex, n), Arrays.copyOfRange(inorder, rootIndex + 1, n));
@@ -59,5 +79,27 @@ public class BuildTree {
             stringBuilder.append(i);
         }
         return stringBuilder.toString();
+    }
+
+    public TreeNode buildTree3(int[] preorder, int[] inorder) {
+        int len = preorder.length;
+        if (len==0) return null;
+        indexMap = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            indexMap.put(inorder[i],i);
+        }
+        return myBuildTree2(preorder,inorder,0,len-1,0,len-1);
+    }
+
+    public TreeNode myBuildTree2(int[] preorder, int[] inorder,int preLeft,int preRight,int inLeft,int inRight) {
+        if (preLeft>preRight || inLeft>inRight) return null;
+
+        int rootVal = preorder[preLeft];
+        int rootIndex = indexMap.get(rootVal);
+        int leftSize = rootIndex - inLeft;
+        TreeNode root = new TreeNode(rootVal);
+        root.left = myBuildTree2(preorder,inorder,preLeft + 1, preLeft + leftSize,inLeft,rootIndex-1);
+        root.right = myBuildTree2(preorder,inorder,preLeft + leftSize + 1, preRight,1+rootIndex, inRight);
+        return root;
     }
 }
